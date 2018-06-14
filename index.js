bdcc7ab00be89bb071516a77ee96bef498fc408f
Kevin Choong <kmaehya@gmail.com>
2018-06-14 20:19:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ function inc(n) {
 function dec(n) {
   return --n;
 }
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function parseInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
